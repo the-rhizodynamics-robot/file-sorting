52b83b76d3b851f33ca23931e52902c85bdb26df
Isaiah Taylor <iwtwb8@gmail.com>
2025-02-12 21:42:56 -0500

    docker working, general debugging

--- work/21/0c525483b1ce13ebb6a9495e8f3052/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 robot_image_sorting.py         --sort_path /mnt/c/Users/iwt/Desktop/file_sorting_test/         --staging_path /mnt/c/Users/iwt/Desktop/sorting_files/zips/         --robot_number 1         --boxes_per_shelf 7
